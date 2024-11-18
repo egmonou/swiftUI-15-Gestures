7f55e5b23f27cff3d704d7ab5872ce66df9e6ab7
monour <mohamednour25@gmail.com>
2024-11-18 16:34:29 +0400

    Initial Commit

---
+++ Gestures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gestures
+//
+//  Created by Mohamed Nour on 18/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Mohamed Nour on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
